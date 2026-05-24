b1ee9182b5f7345eb1b5c4beaf050d4e22ac8e57
Maria Khan <mariakhan200502@gmail.com>
2026-05-24 12:04:29 +0530

    inserted comparison metrics on UI

--- app.py
+++ app.py
@@ -1,66 +1,92 @@
 import streamlit as st
-import joblib
-from tensorflow.keras.models import load_model  
 
+st.set_page_config(
+    page_title="Financial Forecasting Dashboard",
+    layout="wide"
+)
 
+st.title("Financial Forecasting & Model Comparison Dashboard")
 
-st.title("Tesla Stock Prediction App")
+st.markdown("""
+Welcome to the Financial Forecasting Dashboard.
 
-model_choice = st.sidebar.selectbox(
-    "Choose Model",
+Compare Machine Learning and Deep Learning models
+across different financial assets such as:
+
+- Stocks
+- Gold
+- Crude Oil
+""")
+
+asset_choice = st.selectbox(
+    "Choose Asset Type",
     [
+        "Stocks",
+        "Gold",
+        "Crude Oil"
+    ]
+)
+
+# STOCKS SECTION
+if asset_choice == "Stocks":
+
+    company_choice = st.selectbox(
+        "Choose Company",
+        [
+            "Tesla",
+            "Apple",
+            "Amazon"
+        ]
+    )
+
+    st.subheader(f"{company_choice} Analysis Dashboard")
+
+    
+
+# GOLD SECTION
+elif asset_choice == "Gold":
+
+    st.subheader("Gold Price Analysis Dashboard")
+
+# CRUDE OIL SECTION
+elif asset_choice == "Crude Oil":
+
+    st.subheader("Crude Oil Price Analysis Dashboard")
+
+st.info("Coming Soon")
+
+import pandas as pd
+
+results_df = pd.DataFrame({
+    "Model": [
         "Linear Regression",
         "Random Forest",
         "XGBoost",
         "LSTM"
+    ],
+
+    "MAE": [
+        5.7608,
+        6.7108,
+        7.0787,
+        10.1034
+    ],
+
+    "RMSE": [
+        8.0601,
+        9.2664,
+        9.5836,
+        13.7530
+    ],
+
+    "R2 Score": [
+        0.9691,
+        0.9591,
+        0.9563,
+        0.9132
     ]
-)
+})
 
-st.write("Selected Model:", model_choice)
+st.write("## Model Performance Comparison")
 
-lr_model = joblib.load(
-    "models/linear_regression.pkl"
-)
-
-rf_model = joblib.load(
-    "models/random_forest.pkl"
-)
-
-xgb_model = joblib.load(
-    "models/xgboost.pkl"
-)
-
-lstm_model = load_model(
-    "models/lstm_tesla_stock_model.h5"
-)
-
-scaler = joblib.load(
-    "models/scaler.pkl"
-)
-
-st.header("Enter Stock Data")
-
-open_price = st.number_input("Open Price")
-
-high_price = st.number_input("High Price")
-
-low_price = st.number_input("Low Price")
-
-volume = st.number_input("Volume")
-
-predict_button = st.button("Predict Stock Price")
-
-if predict_button:
-
-    input_data = [[
-        volume,
-        open_price,
-        high_price,
-        low_price
-    ]]
-
-    prediction = lr_model.predict(input_data)
-
-    st.success(
-        f"Predicted Closing Price: ${prediction[0]}"
-    )
+st.dataframe(results_df)
